94c381d00758fe63815deba8027fb0e9013a3492
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-05 16:00:14 +0100

    Finished sticky menu

--- 3.StickyMenu/js/app.js
+++ 3.StickyMenu/js/app.js
@@ -3,12 +3,24 @@ $(function() {
 
     let nav = $("nav");
     let menu = $(".menu");
-    let menuPosition = menu.css("top", "");
+    let menuPosition = menu.offset().top;
 
-    console.log(nav, menu, menuPosition)
+    console.log(nav, menu, menuPosition);
 
-    window.on("scroll", function() {
-       console.log("scroll")
+    $(window).on("scroll", function() {
+        if ( $(this).scrollTop() >= menuPosition ) {
+            menu.addClass("sticky")
+        } else {
+            menu.removeClass("sticky")
+        }
+    });
+
+    $(window).on("resize", function() {
+        if ( menu.hasClass("sticky") ) {
+            return menuPosition = menu.offset().top;
+        } else {
+            return menuPosition = nav.offset().top;
+        }
     });
 
 
