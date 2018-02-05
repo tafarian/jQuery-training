f1dfea8757a356ed59b494c754b0c4d1d9f4e5f3
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-05 17:25:19 +0100

    Almost finished progress bar

---
+++ 4.ProgressBar/js/app.js
@@ -0,0 +1,33 @@
+$(function() {
+    console.log("DOM Loaded");
+
+    let buttons = $("button");
+    let progressBar = $(".progress-bar");
+    console.log(buttons, progressBar);
+
+    function animateProgress($param) {
+    }
+
+    buttons.on("click", function() {
+        let width = $(this).data("width");
+        let number = $(this).data("number");
+        let color = $(this).data("color");
+        console.log(width, number, color,)
+
+        if ( number === 1 ) {
+            let bar = $("#bar1").find("span");
+            bar.toggleClass(color);
+            bar.css({width: width});
+        } else if ( number === 2 ) {
+            let bar = $("#bar2").find("span");
+            bar.toggleClass(color);
+            bar.css({width: width});
+        } else if ( number === 3 ) {
+            let bar = $("#bar3").find("span");
+            bar.toggleClass(color);
+            bar.css({width: width});
+        }
+
+    });
+
+});
